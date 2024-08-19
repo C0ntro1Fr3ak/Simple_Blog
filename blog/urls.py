717038fe3b396ed07ed429ac280dacf575ab207b
Robert <rob007.thomson@gmail.com>
2024-08-19 20:35:31 +1200

    all basic creation and updating features added and working

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from blog.views import HomeView
+from blog.views import HomeView, PostDetailView, PostCreateView, PostUpdateView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-
+    path('post_detail/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('post_create/', PostCreateView.as_view(), name='post_create'),
+    path('post_update/<int:pk>/', PostUpdateView.as_view(), name='post_update'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 
 from blog.models import Post
 
@@ -9,3 +9,18 @@ class HomeView(ListView):
     model = Post
     template_name = 'home.html'
     ordering = ['-created_at']
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+class PostCreateView(CreateView):
+    model = Post
+    template_name = 'post_create.html'
+    fields = ['title', 'content', 'category']
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ['title', 'content', 'category']
+    template_name = 'post_update.html'
